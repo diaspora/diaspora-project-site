a804965b8a6ab218e8d7fa998e6a01d0189a4e63
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-08-25 15:15:16 -0700

    remove posts

--- app/helpers/posts_helper.rb
+++
@@ -1,6 +0,0 @@
-module PostsHelper
-  def markdown(text)
-    options = [:hard_wrap, :filter_html, :autolink, :no_intraemphasis, :fenced_code, :gh_blockcode]
-    Redcarpet.new(text, *options).to_html.html_safe
-  end
-end
--- app/models/post.rb
+++
@@ -1,7 +0,0 @@
-class Post < ActiveRecord::Base
-  belongs_to :author
-
-  def to_param
-    "#{id}-#{title.parameterize}"
-  end
-end
