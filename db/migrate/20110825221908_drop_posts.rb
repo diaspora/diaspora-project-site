778a09f7f043b146a349ea23553ca875b55bb71b
danielgrippi <danielgrippi@gmail.com>
2011-09-02 17:32:40 -0700

    added more text, fixed styling, positioning

--- db/migrate/20110825221908_drop_posts.rb
+++
@@ -1,8 +0,0 @@
-class DropPosts < ActiveRecord::Migration
-  def self.up
-    drop_table :posts
-  end
-
-  def self.down
-  end
-end
--- db/migrate/20110825222750_remove_authors.rb
+++
@@ -1,8 +0,0 @@
-class RemoveAuthors < ActiveRecord::Migration
-  def self.up
-    drop_table :authors
-  end
-
-  def self.down
-  end
-end
