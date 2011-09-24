462fe2342cb2ebd75574747300cc88e192b6719e
danielgrippi <danielgrippi@gmail.com>
2011-09-23 21:32:19 -0700

    add commit and contributor count to get_involved

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -6,24 +6,27 @@ function randomOrder(){
 }
 
 $(document).ready(function(){
-  var repo = gh.repo("diaspora", "diaspora");
-  var default_url = "404"//escape("https://joindiaspora.com/images/asterisk.png");
+  var repo = gh.repo("diaspora", "diaspora"),
+             default_url = "404";
+
   repo.contributors(function(data){
-    var  contributors = data['contributors'].sort(randomOrder);
+    var contributors = data['contributors'].sort(randomOrder);
+    var contributions = 0;
+
     $.each(contributors, function(key,val){
       var img = new Image();
-      img.src = "http://gravatar.com/avatar/"+val["gravatar_id"]+"?d=" + default_url;
+      img.src = "http://gravatar.com/avatar/"+val["gravatar_id"]+"?s=60&d=" + default_url;
       img.height = 60;
       img.id = val['login'];
-      img.classList.add("faded")
+      img.title = val['login'];
       img.width = 60;
       img.onload = function(){
         $("#contributors").append($(img).fadeIn("fast"));
       }
-      var number = $('#number').text();
-      number = parseInt(number) + val['contributions']
-      $('#number').text(number);
+      contributions = parseInt(contributions) + val['contributions'];
     });
+
+    $('#contributor-count').hide().html("<strong>" + contributions + "</strong> code commits from <strong>" + data['contributors'].length + "</strong> unique contributors & counting").fadeIn(1000);
   });
 
   $("img.faded").live('mouseover', function(){
