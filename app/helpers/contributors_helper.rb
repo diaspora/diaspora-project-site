39eb40f71697dbf12d92740a406fe28b185b2421
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-09-24 23:45:39 -0700

    update site for diaspora image url

--- app/helpers/contributors_helper.rb
+++ app/helpers/contributors_helper.rb
@@ -1,9 +1,4 @@
 module ContributorsHelper
-  def diaspora_url(contributor)
-    username, server = contributor.diaspora_id.split('@')
-    "http://#{server}/u/#{username}"
-  end
-
   def contributor_type(type)
     case type
     when 'hacker'
--- app/models/contributor.rb
+++ app/models/contributor.rb
@@ -6,4 +6,18 @@ class Contributor < ActiveRecord::Base
   def first_name
     self.name.split(' ').first
   end
+
+  def diaspora_username
+   diaspora_id.split('@').first
+  end
+
+  def diaspora_url
+    username, server = diaspora_id.split('@')
+    "http://#{server}/u/#{username}"
+  end
+
+
+  def diaspora_avatar
+    "#{diaspora_url}/profile_photo"
+  end
 end
