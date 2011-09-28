d25b76b1bedaf67676091e67ef36912d9371e804
Maxwell Salzberg <maxwell@joindiaspora.com>
2011-09-28 15:29:36 -0700

    peters changes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,12 @@ module ApplicationHelper
   def code_link
     "https://github.com/diaspora/diaspora"
   end
+
+  def install_development_enviroment_path
+    'https://github.com/diaspora/diaspora/wiki/Installing-and-Running-Diaspora'
+  end
+
+  def get_satisfaction_link
+    'http://getsatisfaction.com/diaspora'
+  end
 end
