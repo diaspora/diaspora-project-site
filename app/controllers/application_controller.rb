6ccabcd607c2542c9b58ab05ab6024aef4274c18
maxwell <maxwell@maxwell-betty.(none)>
2011-09-28 23:30:24 -0700

    cache money and fix blog link

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-
+  before filter :cache_money
 
 protected
 
@@ -8,5 +8,9 @@ protected
     authenticate_or_request_with_http_basic do |username, password|
       username == ENV['ADMIN_USERNAME'] && password == ENV['ADMIN_PASSWORD']
     end
+  end
+  
+  def cache_money
+    response.headers['Cache-Control'] = 'public, max-age=300' if ENV['cache']
   end
 end
