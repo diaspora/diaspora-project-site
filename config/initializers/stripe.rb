9e5dcfc7b512b2a104cd5a364a4c3759b3886c3f
danielgrippi <danielgrippi@gmail.com>
2011-10-18 17:38:33 -0700

    embed all resources; stripe uses local or env vars

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,4 +1,8 @@
-keys = YAML.load(File.read(File.join(File.dirname(__FILE__), '../stripe.yml')))
+begin
+  stripe = YAML.load(File.read(File.join(File.dirname(__FILE__), '../stripe.yml')))
+rescue
+  stripe = {}
+end
 
-STRIPE_PUBLISHABLE_KEY = keys['publishable']
-Stripe.api_key = keys['secret']
+STRIPE_PUBLISHABLE_KEY = stripe["publishable"] || ENV['stripe_publishable']
+Stripe.api_key = stripe["secret"] || ENV['stripe_secret']
