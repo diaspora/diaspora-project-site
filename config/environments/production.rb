024d05d0f0169706c6aa2929089058b4fdb14eeb
Jonne Haß <me@mrzyx.de>
2013-07-09 23:10:48 +0200

    Update production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-DiasporaProject::Application.configure do
+DiasporaProjectSite::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
