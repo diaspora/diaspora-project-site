0bca60f00f3eb527b4f24d085f8d2ab71b879d33
Jonne Haß <me@mrzyx.de>
2014-02-19 10:06:50 +0100

    new rails_admin initializer

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,37 +1,34 @@
-# RailsAdmin config file. Generated on June 08, 2013 20:52
-# See github.com/sferik/rails_admin for more informations
-
 RailsAdmin.config do |config|
 
+  ### Popular gems integration
 
-  ################  Global configuration  ################
+  ## == Devise ==
+  config.authenticate_with do
+    warden.authenticate! scope: :user
+  end
+  config.current_user_method(&:current_user)
 
-  # Set the admin name here (optional second array element will appear in red). For example:
-  config.main_app_name = ['Diaspora Project Site', 'Admin']
-  # or for a more dynamic name:
-  # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
+  ## == Cancan ==
+  # config.authorize_with :cancan
 
-  # RailsAdmin may need a way to know who the current user is]
-  config.current_user_method { current_user } # auto-generated
+  ## == PaperTrail ==
+  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
 
-  # If you want to track changes on your models:
-  # config.audit_with :history, 'User'
+  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
 
-  # Or with a PaperTrail: (you need to install it first)
-  # config.audit_with :paper_trail, 'User'
+  config.actions do
+    dashboard                     # mandatory
+    index                         # mandatory
+    new
+    export
+    bulk_delete
+    show
+    edit
+    delete
+    show_in_app
 
-  # Display empty fields in index views:
-  # config.compact_show_view = false
-
-  # Number of default rows per-page:
-  # config.default_items_per_page = 20
-
-  # Exclude specific models (keep the others):
-  # config.excluded_models = ['Page', 'User']
-
-  # Include specific models (exclude the others):
-  config.included_models = ['User', 'Blogpost']
-
-  # Label methods for model instances:
-  config.label_methods = [:name, :title, :email]
+    ## With an audit adapter, you can add:
+    # history_index
+    # history_show
+  end
 end
