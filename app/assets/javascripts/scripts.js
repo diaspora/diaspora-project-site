2969ac24b6e48395ec35eb6e24b59825856ce832
EVOKE <evoke.lektrique@gmail.com>
2018-02-07 21:29:48 +0330

    Better style

---
+++ app/assets/javascripts/scripts.js
@@ -0,0 +1,5 @@
+// $(document).ready(function(){
+// 	var colors = ['reef', 'amethyst', 'purple', 'timber', 'orange'];
+// 	var random_color = Math.floor(Math.random() * colors.length);
+// 	$('.hero').addClass(`bg-${colors[random_color]}`);
+// });
