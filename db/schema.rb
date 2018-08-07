3a27831985574ccd3ebc71e641c45735c364207e
Dennis Schubert <mail@dennis-schubert.de>
2018-08-07 08:04:23 +0200

    Checkin a Postgres schema
    because that's what the production site will run on soon.

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,10 @@
 
 ActiveRecord::Schema.define(version: 20141210000000) do
 
-  create_table "assets", force: true do |t|
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "assets", force: :cascade do |t|
     t.string   "title"
     t.boolean  "public"
     t.text     "description"
@@ -26,7 +29,7 @@ ActiveRecord::Schema.define(version: 20141210000000) do
     t.datetime "updated_at"
   end
 
-  create_table "blogposts", force: true do |t|
+  create_table "blogposts", force: :cascade do |t|
     t.string   "title",                      null: false
     t.boolean  "published",  default: false
     t.text     "content",                    null: false
@@ -34,7 +37,7 @@ ActiveRecord::Schema.define(version: 20141210000000) do
     t.datetime "updated_at"
   end
 
-  create_table "planet_entries", force: true do |t|
+  create_table "planet_entries", force: :cascade do |t|
     t.string   "title"
     t.string   "author"
     t.string   "url",          limit: 800
@@ -46,7 +49,7 @@ ActiveRecord::Schema.define(version: 20141210000000) do
     t.datetime "updated_at"
   end
 
-  create_table "planet_feeds", force: true do |t|
+  create_table "planet_feeds", force: :cascade do |t|
     t.string   "url",        limit: 800
     t.string   "site_url",   limit: 800
     t.string   "title"
@@ -54,7 +57,7 @@ ActiveRecord::Schema.define(version: 20141210000000) do
     t.datetime "updated_at"
   end
 
-  create_table "rails_admin_histories", force: true do |t|
+  create_table "rails_admin_histories", force: :cascade do |t|
     t.text     "message"
     t.string   "username"
     t.integer  "item"
@@ -67,7 +70,7 @@ ActiveRecord::Schema.define(version: 20141210000000) do
 
   add_index "rails_admin_histories", ["item", "table", "month", "year"], name: "index_rails_admin_histories", using: :btree
 
-  create_table "users", force: true do |t|
+  create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
